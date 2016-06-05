8021c784d3559609f7f3ececa273f5e87bba8181
hmelo <hans.melo@reclameaqui.com.br>
2016-06-05 19:11:39 -0300

    Criação primeiro teste

--- spec/parse_document_spec.rb
+++ spec/parse_document_spec.rb
@@ -1,6 +1,16 @@
 require 'minitest/spec'
 require 'minitest/autorun'
+require 'nokogiri' 
 require_relative '../lib/parse_document'
-
 describe CharacterSpokenLinesCount::ParseDocument do
+    before do
+        @document = Nokogiri::XML(open('macbeth.xml')) 
+    end
+    
+    describe "#parse" do
+        it "must returned hash"
+        parse_document = CharacterSpokenLinesCount::ParseDocument.new
+        hash_character_count = parse_document.get_hash(@document)
+        hash_character_count["MACBETH"].must_equal 741
+    end
 end
