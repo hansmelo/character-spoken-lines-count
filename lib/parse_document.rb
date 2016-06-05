ab87b3a56bc017f2466712614f297dd21f808654
hmelo <hans.melo@reclameaqui.com.br>
2016-06-05 20:47:05 -0300

    Refatoração da classe parse_document

--- lib/parse_document.rb
+++ lib/parse_document.rb
@@ -11,26 +11,39 @@ module CharacterSpokenLinesCount
         end
     
         def get_hash()
-            characteres = @document.xpath(PATH_XML_CHARACTERS).map { |node| node.children.text }
+            array_characteres = get_array_characteres
+            get_hash_characteres_count(array_characteres)
+        end
+        
+        private
+        def get_array_characteres
+            @document.xpath(PATH_XML_CHARACTERS).map { |node| node.children.text }.select{|character| character != CHARACTER_IGONE}
+        end
+        
+        def get_hash_characteres_count(array_characteres)
             hash_document = {}
-            characteres.each do |name|
-                hash_document[name] = 0 if name != CHARACTER_IGONE
-            end
-
-            hash_document.each do |key, value|
-                nodes_speech = @document.xpath("//SPEAKER[text()='#{key}']")
-                nodes_spoken_line = []
-                nodes_speech.each do |node|
-                    node_next = node.next_element
-                    while(node_next != nil)
-                        nodes_spoken_line << node_next
-                        node_next = node_next.next_element
-                    end
-                end
-                hash_document[key] = nodes_spoken_line.size
+            array_characteres.each do |character|
+                hash_document[character] = count_lines_spoken(character)
             end
             hash_document
         end
+        
+        def count_lines_spoken(character)
+            nodes_speech = get_nodes_speech(character)
+            nodes_spoken_line = []
+            nodes_speech.each do |node|
+                node_next = node.next_element
+                until(node_next.nil?)
+                    nodes_spoken_line << node_next
+                    node_next = node_next.next_element
+                end
+            end
+            nodes_spoken_line.size
+        end
+        
+        def get_nodes_speech(character)
+            @document.xpath("//SPEAKER[text()='#{character}']")
+        end 
     end
     
 end
